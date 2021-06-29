d38719a69496fb2278ea63552a5bd32984267cce
vocart <72550437+vocart@users.noreply.github.com>
2021-06-29 15:51:37 +0200

    django commit

---
+++ generator/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+
+TRAINING_TYPES = [
+        ('KARATE', 'KARATE'),
+        ('STANDARD', 'STANDARD'),
+        ('CARDIO', 'CARDIO'),
+        ('SRETCHING', 'SRETCHING'),
+
+        ]       
+
+TRAINING_DURATION = [
+        ('15', '15'),
+        ('30', '30'),
+        ('45', '45'),
+        ('60', '60'),
+        ]   
+
+class TrainingForm(forms.Form):
+    train_type = forms.CharField(label='Choose the type of training:', widget=forms.RadioSelect(choices = TRAINING_TYPES))
+    train_duration = forms.CharField(label='Choose the duration of training:', widget=forms.RadioSelect(choices = TRAINING_DURATION))
